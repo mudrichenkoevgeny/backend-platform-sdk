5956e3f4b37b08bd8eb25214ce630db4ea6c5d72
Evgeny Mudrichenko <evgeny.mudrichenko@gmail.com>
2026-02-27 23:04:19 +0300

    fix: add Serializable annotation to AvailableAuthProviders

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 allprojects {
     group = "io.github.mudrichenkoevgeny"
-    version = "0.0.12"
+    version = "0.0.13"
 }
 
 subprojects {
--- feature/user/src/main/kotlin/io/github/mudrichenkoevgeny/backend/feature/user/model/auth/AvailableAuthProviders.kt
+++ feature/user/src/main/kotlin/io/github/mudrichenkoevgeny/backend/feature/user/model/auth/AvailableAuthProviders.kt
@@ -1,7 +1,9 @@
 package io.github.mudrichenkoevgeny.backend.feature.user.model.auth
 
 import io.github.mudrichenkoevgeny.shared.foundation.feature.user.domain.model.UserAuthProvider
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class AvailableAuthProviders(
     val primary: List<UserAuthProvider>,
     val secondary: List<UserAuthProvider>
